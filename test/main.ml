6ab32d13052fb4ae7fc13487fcebde49ff7c6f51
Enjie Wang <ew438@cornell.edu>
2023-04-24 16:00:19 -0400

    Modified get_data.ml, main.ml
    - added exceptions to the mli
    - added test cases for calculate_score function

--- test/main.ml
+++ test/main.ml
@@ -20,12 +20,43 @@ let calculate_score_test (name : string) (word_repo : Get_data.t)
     (int_list_to_string (calculate_score word_repo sentence word_list))
     ~printer:string_of_string
 
+let calculate_score_fail_test (name : string) (word_repo : Get_data.t)
+    (sentence : string) (word_list : string list) (expected_output : exn) : test
+    =
+  name >:: fun _ ->
+  assert_raises expected_output (fun () ->
+      calculate_score word_repo sentence word_list)
+
 let algorithm_test =
   [
     calculate_score_test
       "gets the scores for Did you see the ___ run across? with [iron, bubbly] \
        in test_data."
       test_json "Did you see the ___ run across?" [ "iron"; "bubbly" ] "10050";
+    calculate_score_test
+      "gets the scores for How did she ___ the raccoon? with [running, braids, \
+       curious] in test_data."
+      test_json "How did she ___ the raccoon?"
+      [ "running"; "braids"; "curious" ]
+      "1007550";
+    calculate_score_fail_test
+      "gets the exception for How did she ___ the Donkey Kong? with [running, \
+       braids, curious] in test_data."
+      test_json "How did she ___ the Donkey Kong?"
+      [ "running"; "braids"; "curious" ]
+      (InvalidSentence "How did she ___ the Donkey Kong?");
+    calculate_score_fail_test
+      "gets the exception for How did she ___ the raccoon? with [BAHAHA, \
+       braids, curious] in test_data."
+      test_json "How did she ___ the raccoon?"
+      [ "BAHAHA"; "braids"; "curious" ]
+      (InvalidWords [ "BAHAHA"; "braids"; "curious" ]);
+    calculate_score_fail_test
+      "gets the exception for Fake sentence with [BAHAHA, braids, curious] in \
+       test_data."
+      test_json "Fake sentence"
+      [ "BAHAHA"; "braids"; "curious" ]
+      (InvalidWords [ "BAHAHA"; "braids"; "curious" ]);
   ]
 
 let suite = "test suite for Allchat" >::: List.flatten [ algorithm_test ]
