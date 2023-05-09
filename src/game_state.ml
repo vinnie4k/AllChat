f8ee84f3616deed6cad7d9167924356920f15836
Liam Du <ld386@cornell.edu>
2023-05-09 19:22:40 -0400

    Implemented Game_state.update_round
    - Implemented a way to update the current round for game state
    - Modified some mli comments for functions in game state

--- src/game_state.ml
+++ src/game_state.ml
@@ -4,7 +4,7 @@ type game_mode = Toxic | Wholesome
 
 type score_mode = Autoscore | Playscore
 
-type t = {game_mode : game_mode; score_mode : score_mode; game_length: int; num_of_player : int; players : Player.t array; round_scores : int array; current_round : int}
+type t = {game_mode : game_mode; score_mode : score_mode; game_length: int; num_of_player : int; players : Player.t array; round_scores : int array; mutable current_round : int}
 
 let initialize_game game_mode score_mode game_length num_of_players name_lst = 
   {game_mode = game_mode; score_mode = score_mode; game_length = game_length; num_of_player = num_of_players; players = List.map Player.new_player name_lst |> Array.of_list; round_scores = Array.make num_of_players 0; current_round = 1}
@@ -36,4 +36,7 @@ else find_max_score_index round_score_total max_score (index+1)
 
 let get_winner game_state = Array.get game_state.players (find_max_score_index (get_round_score_total game_state) (max_score_finder (get_round_score_total_list game_state)) (0)) |> Player.get_player_name
 
+(*modifying functions*)
 let update_player_score game player_index score = Player.update_score (Array.get game.players player_index) score
+
+let update_round game = game.current_round <- (game.current_round + 1)
--- src/game_state.mli
+++ src/game_state.mli
@@ -6,7 +6,7 @@ type score_mode
 
 type t
 (** The abstract data type representing the the game and its state. It includes the game mode, score mode, number of players, the player themselves, scores for each round for each player, current round, 
-    and whether or not the game ended.*)
+    and whether or not the game ended. The most important features of game_state is to keep track of the players in the game, keep track of rounds, update the player's score based on the given score calculated in get_data and interface, as well as keeping track of rounds and when the game ends.*)
 
 val initialize_game : game_mode -> score_mode -> int -> int -> string list -> t
 (** [initialize_game] n name_lst game_mode score_mode initializes the game with number of players n, each with name in name_lst, with game mode being game_mode and score mode being score_mode. *)
@@ -27,5 +27,10 @@ val get_winner : t -> string
 (** [get_winner] game is the winner of the round, call this function if game ends to determine final winner. *)
 
 val update_player_score : t -> int -> int -> unit
-(** [update_player_score game player_index score] updates each [player_index]'s score based on the input [scores]. *)
+(** [update_player_score game player_index score] updates each [player_index]'s score based on the input [scores].
+    We use player_index to find the name of the player based on its index in the player list array.
+    Call this function when updating each individual player score with the calculated score after the player's turn*)
+
+val update_round : t -> unit
+(** [update_round game round] updates [game]'s current round. Call this function whenever you are moving to the next round *)
 
